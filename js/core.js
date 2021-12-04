49b106a31a0d5a513cb13ce6d4cf525ce9380138
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-04 09:45:09 -0500

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,7 +1,10 @@
+const ace = window.ace;
 var te = window.te;
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/chrome");
-te.theme = "ace/theme/chrome";
-editor.session.setMode("ace/mode/javascript");
-te.mode = "ace/mode/javascript";
-te.mainMenu = "inactive";
+window.addEventListener('load', function() {
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/chrome");
+    te.theme = "ace/theme/chrome";
+    editor.session.setMode("ace/mode/javascript");
+    te.mode = "ace/mode/javascript";
+    te.mainMenu = "inactive";
+});
