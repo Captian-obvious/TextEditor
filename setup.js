7fcd68e409df68143be980ef9a6780b941dde369
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-03 22:29:15 -0500

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,5 @@
+var ace = window.ace
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/chrome");
+editor.session.setMode("ace/mode/javascript");
+var app = document.getElementById('app')
