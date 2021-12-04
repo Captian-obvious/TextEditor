f36e226dab021e7515d6103cba884339713f5aa2
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-03 22:51:27 -0500

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,8 @@
-var ace = window.ace
-var app = document.getElementById('app')
-window.addEventListener('load',function(){
-    app.innerHTML = `
+const ace = window.ace
+var te = {};
+window.addEventListener('load', function() {
+    var container = document.getElementById('app');
+    container.innerHTML = `
     <div id="menu"></div>
     <div id="editor">function foo(items) {
     var x = "All this is syntax highlighted";
@@ -10,5 +11,8 @@ window.addEventListener('load',function(){
     `
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/chrome");
+    te.theme = "ace/theme/chrome";
     editor.session.setMode("ace/mode/javascript");
+    te.mode = "Javascript";
+    te.mainMenu = {};
 });
