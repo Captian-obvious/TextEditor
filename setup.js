6e69ad4e6a2f578b8438675f52e93f0202301c40
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-03 22:32:49 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -3,3 +3,12 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/chrome");
 editor.session.setMode("ace/mode/javascript");
 var app = document.getElementById('app')
+window.addEventListener('load',function(){
+    app.innerHTML = `
+    <div id="menu"></div>
+    <div id="editor">function foo(items) {
+    var x = "All this is syntax highlighted";
+    return x;
+    }</div>
+    `
+});
