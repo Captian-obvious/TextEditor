0788aa678effba45e0b0d6f1c32a32db22f04eaa
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-04 09:34:56 -0500

    Create core.js

---
+++ js/core.js
@@ -0,0 +1,7 @@
+var te = window.te;
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/chrome");
+te.theme = "ace/theme/chrome";
+editor.session.setMode("ace/mode/javascript");
+te.mode = "ace/mode/javascript";
+te.mainMenu = "inactive";
