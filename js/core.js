a70aaa78d24313fc864092a95b375424b9f28582
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-07 09:43:19 -0500

    Update core.js

--- js/core.js
+++ js/core.js
@@ -6,5 +6,5 @@ window.addEventListener('load', function() {
     te.theme = "ace/theme/chrome";
     editor.session.setMode("ace/mode/javascript");
     te.mode = "ace/mode/javascript";
-    te.mainMenu = "inactive";
+    te.mainMenu.status = "inactive";
 });
