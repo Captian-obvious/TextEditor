ca73ea890333daf3a375cfad6fc5d581ce6fdc87
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-07 10:31:24 -0500

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,6 +1,14 @@
 const ace = window.ace;
 var te = window.te;
 window.addEventListener('load', function() {
+    var container = document.getElementById('app');
+    container.innerHTML = `
+    <div id="menu"></div>
+    <div id="editor">function foo(items) {
+    var x = "All this is syntax highlighted";
+    return x;
+    }</div>
+    `
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/chrome");
     te.theme = "ace/theme/chrome";
