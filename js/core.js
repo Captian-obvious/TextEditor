200333b379e97b5cca51f980d1affd62ad9ee9e1
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-09 12:36:36 -0500

    Update core.js

--- js/core.js
+++ js/core.js
@@ -11,8 +11,5 @@ window.addEventListener('load', function() {
     `
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/chrome");
-    te.theme = "ace/theme/chrome";
     editor.session.setMode("ace/mode/javascript");
-    te.mode = "ace/mode/javascript";
-    te.mainMenu.status = "inactive";
 });
