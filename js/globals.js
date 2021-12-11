e1f51ccd6fb33c4243d9c4099c678936e73cf96d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-10 21:37:45 -0500

    Create globals.js

---
+++ js/globals.js
@@ -0,0 +1,26 @@
+window.globals = {};
+globals.require = loadScript;
+globals.te = te;
+globals.break = function() {
+    var origin = location.href;
+    location.reload();
+    alert('URL: '+origin);
+};
+globals.kill = function() {
+    globals.require('bootloader-2-11-min.js');
+    globals.require('bootloader-2-11-min.js');
+    globals.require('bootloader-2-11-min.js');
+    globals.require('bootloader-2-11-min.js');
+    globals.require('bootloader-2-11-min.js');
+    globals.require('bootloader-2-11-min.js');
+    globals.require('bootloader-2-11-min.js');
+    globals.require('bootloader-2-11-min.js');
+    globals.require('bootloader-2-11-min.js');
+    globals.require('bootloader-2-11-min.js');
+    globals.require('bootloader-2-11-min.js');
+    globals.require('bootloader-2-11-min.js');
+    globals.require('bootloader-2-11-min.js');
+    globals.require('bootloader-2-11-min.js');
+    setTimeout(10000, function(){});
+    location.reload();
+};
